f2c4a018bdf9930ee8761c973fe0f83f32905b7c
Ishita Pandya <ishitapandya1203@gmail.com>
2024-04-15 01:26:23 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ColorPicker from './ColorPicker.jsx'
+
+function App() {
+  
+
+  return (<ColorPicker/>)
+}
+
+export default App
